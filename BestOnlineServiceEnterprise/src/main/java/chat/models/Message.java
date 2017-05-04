9aaab2c11c8a5856b99134cbcc72772fb0d6da61
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-05-04 20:42:48 +0300

    TASK_0 initial project

---
+++ BestOnlineServiceEnterprise/src/main/java/chat/models/Message.java
@@ -0,0 +1,11 @@
+package chat.models;
+
+/**
+ * 04.05.2017
+ * Message
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class Message {
+}
---
+++ BestOnlineServiceEnterprise/src/main/java/dao/BaseDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+/**
+ * 04.05.2017
+ * BaseDao
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public interface BaseDao<T> {
+
+}
