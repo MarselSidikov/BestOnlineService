735b92ac83977919070225a631a7f6bc8c739828
VikaNoumenko <vika.noumenko@gmail.com>
2017-05-09 19:36:54 +0400

    Current modifications in DAO

--- BestOnlineServiceEnterprise/src/main/java/Main.java
+++ BestOnlineServiceEnterprise/src/main/java/Main.java
@@ -13,9 +13,12 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ru.itis.spring\\context.xml");
         ChatDaoJdbcImpl chatDao = context.getBean(ChatDaoJdbcImpl.class);
-//        List<Chat> chatList = (List<Chat>) chatDao.add();
+
+//        List<Chat> chatList = chatDao.add();
 //        System.out.println(chatList);
-        MessageDaoJdbcImpl messageDao = context.getBean(MessageDaoJdbcImpl.class);
+//        MessageDaoJdbcImpl messageDao = context.getBean(MessageDaoJdbcImpl.class);
+//        List<Message> messageList = messageDao.add();
+//        System.out.println(messageList);
 
 
     }
