d1fa961d0aa422dbe8c2b3c2a7d605339f981fff
VikaNoumenko <vika.noumenko@gmail.com>
2017-05-22 22:05:55 +0400

    Unsolicited commit

---
+++ BestOnlineServiceEnterprise/src/main/java/dao/test/ChatDaoNamedJdbcImplTest.java
@@ -0,0 +1,33 @@
+package dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChatDaoNamedJdbcImplTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void find() throws Exception {
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void findAll() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+}
