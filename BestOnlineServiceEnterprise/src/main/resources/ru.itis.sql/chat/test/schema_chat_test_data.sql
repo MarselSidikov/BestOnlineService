fc1894ba986adf82f8767cb5d3bae6be35d574e0
VikaNoumenko <vika.noumenko@gmail.com>
2017-05-25 10:54:11 +0400

    Commit

--- BestOnlineServiceEnterprise/src/main/resources/ru.itis.sql/chat/test/schema_chat_test_data.sql
+++ BestOnlineServiceEnterprise/src/main/resources/ru.itis.sql/chat/test/schema_chat_test_data.sql
@@ -1,10 +1,7 @@
 INSERT INTO chat(creator_id, name)
-VALUES ('1', 'Marcel');
+VALUES ('1', 'Chat1');
 INSERT INTO chat(creator_id, name)
-VALUES ('2','Ayaz' );
+VALUES ('2','Chat2' );
 INSERT INTO chat(creator_id, name)
-VALUES ('3', 'Denis');
+VALUES ('3', 'Chat3');
 INSERT INTO chat(creator_id, name)
-VALUES ('4', 'Ayvar');
-INSERT INTO chat(creator_id, name)
-VALUES ('5', 'Arthur');
